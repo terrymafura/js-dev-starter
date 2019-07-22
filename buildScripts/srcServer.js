4a8c22c36b258962202d1ab4a11b27a277c98e35
Terry Mafura <maffsojah1@gmail.com>
2019-07-22 11:02:12 +0200

    Add mock Users api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,16 @@ app.get('/', (req, res) => {
 });
 
 
+app.get('/users', (req, res) => {
+    // Hard coding for simplicity. Pretend this hits a real database
+    res.json([
+        {"id":1,"firstName":"Terry","lastName":"Mafura","email":"terry@yahoo.com"},
+        {"id":2,"firstName":"John","lastName":"Banda","email":"johnb@gmail.com"},
+        {"id":3,"firstName":"Mary","lastName":"Mariah","email":"marym@yahoo.com"}
+    ]);
+});
+
+
 app.listen(port, (err) => {
     if(err) {
         console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import './index.css'
+import {getUsers} from "./api/userApi";
 
-import numeral from 'numeral';
+// Populate a table of users via API call
+getUsers().then(result => {
+    let usersBody = "";
 
-/* eslint-disable no-console */
+    result.forEach(user => {
+        usersBody+= `<tr>
+            <td><a href="#" data-id=""${user.id} class="deleteUser">Delete</td>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+            </tr>
+        `
+    });
 
-const courseValue = numeral(1000).format('$0,0.00');
-// debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`);
+    global.document.getElementById('users').innerHTML = usersBody;
+});
