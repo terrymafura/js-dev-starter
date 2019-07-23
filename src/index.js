2d11410c923602eb08172848c4a31b867171875d
Terry Mafura <maffsojah1@gmail.com>
2019-07-23 12:13:52 +0200

    manipulate data via JSON Server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from "./api/userApi";
+import {getUsers, deleteUser} from "./api/userApi";
 
 // Populate a table of users via API call
 getUsers().then(result => {
@@ -15,4 +15,18 @@ getUsers().then(result => {
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    // Must use array.from to create a real array from the DOM collection
+    // getElementByClassName only returns an "array like" object
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes['data-id'].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
